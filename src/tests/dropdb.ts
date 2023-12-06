018dc173ca3ab2b1af97250c394dcb9c64730618
David Hong <davidbor@net.chalmers.se>
2023-12-06 14:24:09 +0100

    #4 Add jest unit tests for users-controller  This commit will add tests for createUser, login, and passwordHash.

--- src/tests/dropdb.ts
+++ src/tests/dropdb.ts
@@ -1,26 +1,30 @@
-import mongoose from"mongoose";
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
-// Variables
-let mongoURI = process.env.MONGODB_URI;
+let mongo: MongoMemoryServer | undefined;
 
-// Checking to see if the MONGODB_URI is entered
-if (!mongoURI) {
-    console.error("MongoDB_URI is required to drop the database, but is not entered.");
-    process.exit(1);
-}
+export const setUp = async (): Promise<void> => {
+  mongo = await MongoMemoryServer.create();
+  const url = mongo.getUri();
 
-// Drop database
-mongoose.connect(mongoURI).catch(function (err) {
-    // The provided URI is wrong or does not exist.
-    if (err) {
-        console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
-        console.error(err.stack);
-        process.exit(1);
+  await mongoose.connect(url);
+};
+
+export const dropDatabase = async (): Promise<void> => {
+  if (mongo) {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongo.stop();
+  }
+};
+
+export const dropCollections = async (): Promise<void> => {
+  if (mongo) {
+    const collections = mongoose.connection.collections;
+
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany();
     }
-});
-
-// Successful case
-mongoose.connection.dropDatabase().then(function () {
-    console.log(`Dropped database: ${mongoURI}`);
-    process.exit(0);
-});
+  }
+};
--- src/utilities/types-utils.ts
+++ src/utilities/types-utils.ts
@@ -1,7 +1,14 @@
-export type MessageData={ [key: string]: unknown };
+export type MessageData = { [key: string]: unknown };
 export type RequestInfo = {
-    user?: { id: string; email: string; userType: string;admin:boolean};
-    requestID?:string;
-  };
-export type MessagePayload={responseTopic:string,payload:MessageData,requestInfo:RequestInfo};
-export type MessageHandler= (data:MessageData,requestInfo:RequestInfo) => Promise<unknown>;
+  user?: { id: string; email: string; userType: string; admin: boolean };
+  requestID?: string;
+};
+export type MessagePayload = {
+  responseTopic: string;
+  payload: MessageData;
+  requestInfo: RequestInfo;
+};
+export type MessageHandler = (
+  data: MessageData,
+  requestInfo: RequestInfo
+) => Promise<unknown>;
